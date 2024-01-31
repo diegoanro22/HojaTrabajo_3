4612b715bfa43506c4afbd760fc8f8728eb787ec
GenserCatalan <gensercatalanandre@gmail.com>
2024-01-30 20:16:30 -0600

    Se agrego el algoritmo Merge Sort

--- GnomeSort.java
+++ GnomeSort.java
@@ -1,3 +1,8 @@
+/*Universidad del Valle de Guatemala 
+ *Genser Andree - 23401
+ *Diego Rosales - 23258
+*/
+
 public class GnomeSort<T extends Comparable<T>> implements iSort<T> {
 
     public T[] sort(T[] array) {
