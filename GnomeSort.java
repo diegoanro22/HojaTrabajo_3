9c372ee4cbb59fb78801ae2c25e5438122b8fa47
GenserCatalan <gensercatalanandre@gmail.com>
2024-01-30 19:46:39 -0600

    Se agrego la clase de ordenamiento Gnome Sort

---
+++ GnomeSort.java
@@ -0,0 +1,20 @@
+public class GnomeSort<T extends Comparable<T>> implements iSort<T> {
+
+    public T[] sort(T[] array) {
+        int index = 0;
+
+        //Algoritmo para ordenar la lista en orden de menor a mayor Gnome Sort
+        while (index < array.length) {
+            if (index == 0 || array[index - 1].compareTo(array[index]) <= 0) {
+                index++;
+            } else {
+                T temp = array[index - 1];
+                array[index - 1] = array[index];
+                array[index] = temp;
+                index--;
+            }
+        }
+
+        return array;
+    }
+}
